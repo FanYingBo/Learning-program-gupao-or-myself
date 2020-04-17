6a4e43faacc5181b585f6b6d8f7702374b1514ac
FanYingBo <15216814563@163.com>
2020-04-17 15:13:55 +0800

    不同方式构建MBeanServer

--- gupao-study-jdk/src/main/java/com/study/jdk5/lang/management/managementfactory/MBeanClientDemo.java
+++ gupao-study-jdk/src/main/java/com/study/jdk5/lang/management/managementfactory/MBeanClientDemo.java
@@ -1,6 +1,7 @@
 package com.study.jdk5.lang.management.managementfactory;
 
 
+import javax.management.JMX;
 import javax.management.MBeanServerConnection;
 import javax.management.ObjectName;
 import javax.management.remote.JMXConnector;
@@ -14,14 +15,19 @@ import java.util.Scanner;
 public class MBeanClientDemo {
 
     public static void main(String[] args) throws Exception {
-        JMXServiceURL jmxServiceURL = new JMXServiceURL("service:jmx:rmi://127.0.0.1:8888/stub/rO0ABXNyAC5qYXZheC5tYW5hZ2VtZW50LnJlbW90ZS5ybWkuUk1JU2VydmVySW1wbF9TdHViAAAAAAAAAAICAAB4cgAaamF2YS5ybWkuc2VydmVyLlJlbW90ZVN0dWLp/tzJi+FlGgIAAHhyABxqYXZhLnJtaS5zZXJ2ZXIuUmVtb3RlT2JqZWN002G0kQxhMx4DAAB4cHc4AAtVbmljYXN0UmVmMgAADTE2OS4yNTQuMTguNDAAACK4V6sx1+47v4Zit0xkAAABcYIlxUmAAQB4");
+        // url 为MBean服务启动注册的url
+        JMXServiceURL jmxServiceURL = new JMXServiceURL("service:jmx:rmi://localhost:1010/jndi/rmi://localhost:8888/jmxrmi");
         JMXConnector jmxConnector = JMXConnectorFactory.connect(jmxServiceURL);
         MBeanServerConnection mBeanServerConnection = jmxConnector.getMBeanServerConnection();
         ObjectName objectName = new ObjectName("test object:name=test name");
         Scanner scanner = new Scanner(System.in);
         String name = null;
         while((name = scanner.nextLine()) != null){
-            mBeanServerConnection.invoke(objectName,"printHello",new String[]{name},new String[]{"java.lang.String"});
+            // 第一种方式
+//            mBeanServerConnection.invoke(objectName,"printHello",new String[]{name},new String[]{"java.lang.String"});
+            // 第二种方式
+            TestObjectMXBean testObjectMXBean = JMX.newMBeanProxy(mBeanServerConnection, objectName, TestObjectMXBean.class);
+            testObjectMXBean.printHello(name);
         }
 
     }
