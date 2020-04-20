07583ad7b241e67de5a08b866c88180b11966e01
FanYingBo <15216814563@163.com>
2020-04-20 13:21:30 +0800

    FunctionInterface

--- gupao-study-jdk/src/main/java/com/study/jdk8/util/function/function/FunctionDemo.java
+++ gupao-study-jdk/src/main/java/com/study/jdk8/util/function/function/FunctionDemo.java
@@ -1,9 +1,16 @@
 package com.study.jdk8.util.function.function;
 
+import java.util.function.Function;
+
 /**
  *
  * @since 1.8
  * @see java.util.function.Function
  */
 public class FunctionDemo {
+
+    public static void main(String[] args) {
+        Function<Integer,String> function = r -> "first";
+        System.out.println(function.apply(1));
+    }
 }
--- gupao-study-jdk/src/main/java/com/study/jdk8/util/function/predicate/PredicateDemo.java
+++ gupao-study-jdk/src/main/java/com/study/jdk8/util/function/predicate/PredicateDemo.java
@@ -1,4 +1,21 @@
 package com.study.jdk8.util.function.predicate;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *   判断条件
+ *  {@link  java.util.function.Predicate}
+ */
 public class PredicateDemo {
+
+    @Test
+    public void testPredicate(){
+        List<String> strings = Arrays.asList(new String[]{"tom","jeck","ben","tony"});
+        List<String> collect = strings.stream().filter(str -> !str.equals("tom")).collect(Collectors.toList());
+        System.out.println(collect);
+    }
 }
