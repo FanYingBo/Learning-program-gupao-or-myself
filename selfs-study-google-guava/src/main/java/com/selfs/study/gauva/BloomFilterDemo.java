384e211394839a828f9eb954ad634ea9d9dd9afb
FanYingBo <15216814563@163.com>
2020-04-21 11:56:02 +0800

    BloomFilter desc

--- selfs-study-google-guava/src/main/java/com/selfs/study/gauva/BloomFilterDemo.java
+++ selfs-study-google-guava/src/main/java/com/selfs/study/gauva/BloomFilterDemo.java
@@ -8,11 +8,27 @@ import org.junit.jupiter.api.Test;
  */
 import java.nio.charset.Charset;
 
+/**
+ * 维基百科
+ * http://en.wikipedia.org/wiki/File:Bloom_filter_fp_probability.svg
+ * 布隆过滤器的证明过程
+ */
 public class BloomFilterDemo {
 
     @Test
     public void BloomFilter(){
-        BloomFilter<CharSequence> bloomFilter = BloomFilter.create(Funnels.stringFunnel(Charset.forName("UTF-8")), 100000, 0.1);
+        int expectedInsertions = 6;
+        double falsePositive = 0.0001;
+        /**
+         * 1. 通过插入次数 n 和false positive 计算数组大小m
+         * 2.通过数组大小m和插入次数n 来计算hash次数 k
+         */
+        BloomFilter<CharSequence> bloomFilter = BloomFilter.create(Funnels.stringFunnel(Charset.forName("UTF-8")), expectedInsertions, falsePositive);
+        /**
+         * (过程复杂)
+         * 1.多次hash
+         * 2.hash 值对BitArray的大小m 取模 判断当前hash位是否是0 若不是则返回true，若是则返回false
+         */
         bloomFilter.put("tom");
         bloomFilter.put("jeck");
         bloomFilter.put("tony");
