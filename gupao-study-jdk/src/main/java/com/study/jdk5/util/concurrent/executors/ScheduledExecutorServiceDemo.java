284fc7c3796ee9052cd40a7c371b0c2ecfc23d09
FanYingBo <15216814563@163.com>
2020-05-06 14:11:02 +0800

    ScheduledExecutorServiceDemo

---
+++ gupao-study-jdk/src/main/java/com/study/jdk5/util/concurrent/executors/ScheduledExecutorServiceDemo.java
@@ -0,0 +1,20 @@
+package com.study.jdk5.util.concurrent.executors;
+
+import java.util.concurrent.*;
+
+/**
+ *  可以用於延時執行的一次性定時器任務
+ */
+public class ScheduledExecutorServiceDemo {
+
+    private static final ScheduledExecutorService schedule = new ScheduledThreadPoolExecutor(2, new ThreadPoolExecutor.CallerRunsPolicy());
+
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        schedule.schedule(()->{
+            System.out.println("execute delay " + (System.currentTimeMillis() - startTime)/1000);
+        },2, TimeUnit.SECONDS);
+        schedule.submit(()-> System.out.println("execute task"));
+        schedule.shutdown();
+    }
+}
