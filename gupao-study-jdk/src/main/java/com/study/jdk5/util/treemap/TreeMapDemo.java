c25cac0a44384d9c4bc75d6d07634cddd50a3832
Fanyingbo <15216814563@163.com>
2020-06-29 17:33:59 +0800

    TreeMap higherEntry 与 floorEntry的用法

--- gupao-study-jdk/src/main/java/com/study/jdk5/util/treemap/TreeMapDemo.java
+++ gupao-study-jdk/src/main/java/com/study/jdk5/util/treemap/TreeMapDemo.java
@@ -1,10 +1,12 @@
 package com.study.jdk5.util.treemap;
 
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
 /**
+ * TreeMap 可以用于有序的数据结构，可以方便取得所在的区间
  * @since 1.2
  * @see java.util.TreeMap  红黑树
  */
@@ -21,7 +23,10 @@ public class TreeMapDemo {
         treeMap.get(1);
         treeMap.remove(2);
 
-
+        Map.Entry<Integer, String> higherEntry = treeMap.higherEntry(2);
+        System.out.println(higherEntry);
+        Map.Entry<Integer, String> floorEntry = treeMap.floorEntry(1);// 小于最小值则取到空值
+        System.out.println(floorEntry);
         Set<Integer> integers = treeMap.keySet();
         Iterator<Integer> iterator = integers.iterator();
         while(iterator.hasNext()){
