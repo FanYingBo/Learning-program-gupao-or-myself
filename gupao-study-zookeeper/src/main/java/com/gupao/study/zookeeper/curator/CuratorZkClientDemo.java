9183023419fb57f9fac5a74c4b4a7f1b7f36ceea
Fanyingbo <15216814563@163.com>
2020-07-15 21:28:18 +0800

    ZK 客户端 Curator

--- gupao-study-zookeeper/src/main/java/com/gupao/study/zookeeper/curator/CuratorZkClientDemo.java
+++ gupao-study-zookeeper/src/main/java/com/gupao/study/zookeeper/curator/CuratorZkClientDemo.java
@@ -2,15 +2,24 @@ package com.gupao.study.zookeeper.curator;
 
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryUntilElapsed;
 
 public class CuratorZkClientDemo {
 
     public static void main(String[] args) {
         //
-        CuratorFramework build = CuratorFrameworkFactory.builder()
-                .connectString("")
-                .connectionTimeoutMs(3000)
+        CuratorFramework curatorFramework = CuratorFrameworkFactory.builder()
+                .connectString("192.168.8.156:2181")
+                .retryPolicy(new RetryUntilElapsed(5, 3000))
+                .connectionTimeoutMs(30000)
                 .build();
+
+        try {
+            curatorFramework.start();
+            curatorFramework.create().forPath("/curator","first".getBytes());
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 
 }
