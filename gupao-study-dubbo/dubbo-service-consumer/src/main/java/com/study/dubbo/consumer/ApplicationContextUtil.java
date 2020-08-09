c25b2ac4635f18f6b7b9d4e8252dc33dec075878
Fanyingbo <15216814563@163.com>
2020-08-09 22:43:54 +0800

    Main.main 启动Dubbo Consumer

---
+++ gupao-study-dubbo/dubbo-service-consumer/src/main/java/com/study/dubbo/consumer/ApplicationContextUtil.java
@@ -0,0 +1,22 @@
+package com.study.dubbo.consumer;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class ApplicationContextUtil implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ApplicationContextUtil.applicationContext = applicationContext;
+    }
+
+    public static <T> T getBean(Class<T> clazz){
+        return applicationContext.getBean(clazz);
+    }
+
+    public static Object getBean(String name){
+        return applicationContext.getBean(name);
+    }
+}
--- gupao-study-dubbo/dubbo-service-consumer/src/main/java/com/study/dubbo/consumer/OrderServiceConsumerMainStart.java
+++ gupao-study-dubbo/dubbo-service-consumer/src/main/java/com/study/dubbo/consumer/OrderServiceConsumerMainStart.java
@@ -1,22 +1,34 @@
 package com.study.dubbo.consumer;
 
 import com.study.dubbo.IPayService;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.apache.dubbo.container.Main;
 
 import java.io.IOException;
 
 //@Configuration
 //@EnableDubbo(scanBasePackages = "com.study.dubbo")
 //@PropertySource("classpath:/dubbo-consumer.properties")
-public class OrderServiceConsumerMainStart {
+public class OrderServiceConsumerMainStart{
 
     public static void main(String[] args) throws IOException {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("consumer.xml");
-        context.start();
+        System.setProperty("dubbo.spring.config","consumer.xml");
+        new Thread(()->{
+            Main.main(new String[]{"log4j","spring"});
+        }).start();
+
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("consumer.xml");
+//        context.start();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         for(int i = 0;i < 5;i++){
-            IPayService alipayService = (IPayService)context.getBean("alipayService");
+            IPayService alipayService = (IPayService)ApplicationContextUtil.getBean("alipayService");
             System.out.println(alipayService.placeAnOrder("newGift","新手礼包",'$',1.99f+i));
         }
-        System.in.read();
+//        System.in.read();
     }
+
 }
