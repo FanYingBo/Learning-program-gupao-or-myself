1c251e536d215e59c844eead1994d59a094d52c7
Fanyingbo <15216814563@163.com>
2020-08-20 19:43:57 +0800

    Dubbo 中对SpringAwareProcessor的运用

--- gupao-study-dubbo/dubbo-service-consumer/src/main/java/com/study/dubbo/consumer/ApplicationContextUtil.java
+++ gupao-study-dubbo/dubbo-service-consumer/src/main/java/com/study/dubbo/consumer/ApplicationContextUtil.java
@@ -4,6 +4,17 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+/**
+ *  @see org.springframework.context.EnvironmentAware
+ *  @see org.springframework.context.EmbeddedValueResolverAware
+ *  @see org.springframework.context.ResourceLoaderAware
+ *  @see org.springframework.context.ApplicationEventPublisherAware
+ *  @see org.springframework.context.MessageSourceAware
+ *  @see org.springframework.context.ApplicationContextAware
+ *  @see org.springframework.context.support.AbstractApplicationContext#refresh()
+ * 在进行Bean解析时候会通过Processor来判断继承关系。让后调用指定的方法设定需要的bean
+ * {@link org.springframework.context.support.ApplicationContextAwareProcessor}
+ */
 public class ApplicationContextUtil implements ApplicationContextAware {
 
     private static ApplicationContext applicationContext;
