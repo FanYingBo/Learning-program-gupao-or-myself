6024af4b8f4bd1dd5985282761e02d955792c495
Fanyingbo <15216814563@163.com>
2020-08-24 11:46:16 +0800

    kafka rename

--- gupao-study-kafka/src/main/java/com/study/gupao/kafka/KafkaPartitionDemo.java
+++ gupao-study-kafka/src/main/java/com/study/gupao/kafka/KafkaPartitionDemo.java
@@ -5,7 +5,7 @@ import org.apache.kafka.common.Cluster;
 
 import java.util.Map;
 
-public class KakfaPartitionDemo implements Partitioner {
+public class KafkaPartitionDemo implements Partitioner {
     public int partition(String s, Object o, byte[] bytes, Object o1, byte[] bytes1, Cluster cluster) {
         return 0;
     }
