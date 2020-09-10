b055035e32f795214fddcec54d2dcc7c14b5266e
Fanyingbo <15216814563@163.com>
2020-09-10 20:55:10 +0800

    通过IBM HeapAnalyzer 分析内存溢出

--- self-study-jvm/src/main/java/com/study/selfs/jvm/OutOfMemoryDemo.java
+++ self-study-jvm/src/main/java/com/study/selfs/jvm/OutOfMemoryDemo.java
@@ -6,7 +6,7 @@ import java.util.Map;
 /**
  *
  * JVM opt  -Xmx1M -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=D:\testdumpfile.log
- *
+ * IBM HeapAnalyzer 分析
  * java.lang.OutOfMemoryError: Java heap space
  * Dumping heap to D:\testdumpfile.log ...
  * Heap dump file created [1471347 bytes in 0.119 secs]
@@ -15,21 +15,26 @@ public class OutOfMemoryDemo {
 
     public static void main(String[] args) {
         TestClass testClass = new TestClass();
-        testClass.put(2,new byte[1024 * 1024]);
-        testClass.put(2,new byte[1024 * 1024]);
+        for(int i = 0;i < 100000;i++){
+            testClass.put(i,new TestEntity());
+        }
     }
 
 }
 
 class TestClass{
 
-    public Map<Integer,byte[]> dataMap;
+    public Map<Integer,TestEntity> dataMap;
 
     public TestClass() {
         this.dataMap = new HashMap<>();
     }
 
-    public void put(int num, byte[] bytes ){
-        dataMap.put(num,bytes);
+    public void put(int num, TestEntity entity ){
+        dataMap.put(num,entity);
     }
+}
+
+class TestEntity{
+
 }
